2d52ea582bb47d86cdb2123cef6ac95292baf55d
limbo <pythonsuper@gmail.com>
2023-01-05 19:00:43 +0800

    test

--- chapter02/formlogin/src/main/java/org/javaboy/formlogin/LoginController.java
+++ chapter02/formlogin/src/main/java/org/javaboy/formlogin/LoginController.java
@@ -7,15 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.servlet.http.HttpServletRequest;
 import java.security.Principal;
 
-/**
- * @author 江南一点雨
- * @微信公众号 江南一点雨
- * @网站 http://www.itboyhub.com
- * @国际站 http://www.javaboy.org
- * @微信 a_java_boy
- * @GitHub https://github.com/lenve
- * @Gitee https://gitee.com/lenve
- */
 @RestController
 public class LoginController {
     @RequestMapping("/index")
